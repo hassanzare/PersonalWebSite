c4c33ab5a898b027740b1bc484bdf86bdec86a8d
hassan <hassan@yahoo.com>
2023-05-14 07:50:33 +0330

    Saidedrawers

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -1,28 +1,45 @@
+import React, { useState, useEffect } from "react";
+
 import { AppBar, Toolbar, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
 import Sidedrawers from "./Sidedrawers";
 
 const Header = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (value) => {
+    setDrawerOpen(value);
+  };
+
   return (
     <AppBar
       position="fixed"
       style={{
-        backgroundColor: "#2f2f2f",
+        backgroundColor: "#81728a",
         boxShadow: "none",
         Padding: "10px 0px",
       }}
     >
       <Toolbar>
         <div className="header_logo">
-          <div className="font_righteous header_logo_venue">The Venue</div>
-          <div className="header_logo_title">Musical Events</div>
+          <div className="font_righteous header_logo_venue">Hasan Zare</div>
+          <div className="header_logo_title">
+            یک برنامه نویس خود آموزکه همواره در حال یادگیری هست
+          </div>
         </div>
 
-        <IconButton aria-label="Menu" color="inherit">
+        <IconButton
+          aria-label="Menu"
+          color="inherit"
+          onClick={() => toggleDrawer(true)}
+        >
           <MenuIcon />
         </IconButton>
-        <Sidedrawers />
+        <Sidedrawers
+          open={drawerOpen}
+          onClose={(value) => toggleDrawer(value)}
+        />
       </Toolbar>
     </AppBar>
   );
--- src/components/header_footer/Sidedrawers.js
+++ src/components/header_footer/Sidedrawers.js
@@ -1,5 +1,29 @@
-const Sidedrawers = () => {
-  return <>hello</>;
+import { Drawer, List, ListItem } from "@mui/material";
+
+const Sidedrawers = (props) => {
+  const links = [
+    { where: "about", value: "درباره من" },
+    { where: "contact", value: "ارتباط با من" },
+    { where: "favorite", value: "علاقه مندی ها" },
+    { where: "experience", value: "تجربه ها" },
+    { where: "projects", value: "پروژ ها" },
+  ];
+
+  const renderItem = (item) => (
+    <ListItem button onClick={() => alert(item.where)} key={item.where}>
+      {item.value}
+    </ListItem>
+  );
+
+  return (
+    <Drawer
+      anchor="right"
+      open={props.open}
+      onClick={() => props.onClose(false)}
+    >
+      <List>{links.map((item) => renderItem(item))}</List>
+    </Drawer>
+  );
 };
 
 export default Sidedrawers;
